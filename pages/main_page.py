f1c7f12f610ac97bc8b0ab5cedae173810cb1518
Maria Vafolomeeva <ewds.war@yandex.ru>
2025-03-03 23:41:56 +0300

    добавлен main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,51 +1,28 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from .base_page import BasePage
 
 
-class MainPage:
-    URL = "https://qa-scooter.praktikum-services.ru/"
-
-    ORDER_BUTTON_TOP = (By.XPATH, "//button[@class='Button_Button__ra12g']")
-    ORDER_BUTTON_BOTTOM = (By.XPATH, "//button[contains(@id893731730 (@class), 'Button_Middle__1CSJM')]")
-    SCOOTER_LOGO = (By.XPATH, "//a[@class='Header_LogoScooter__3lsAR']")
-    YANDEX_LOGO = (By.XPATH, "//a[@class='Header_LogoYandex__3TSOI']")
-    FAQ_QUESTIONS = (By.XPATH, "//div[@class='accordion__item']")
-    FAQ_ANSWERS = (By.XPATH, "//div[@class='accordion__panel']")
-
-    def __init__(self, driver):
-        self.driver = driver
+class MainPage(BasePage):
+    ORDER_BUTTON_TOP = (By.XPATH, "//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать']")
+    ORDER_BUTTON_BOTTOM = (By.XPATH, "//button[contains(@class, 'Button_Button__ra12g') and text()='Заказать']")
+    YANDEX_LOGO = (By.XPATH, "//a[@href='https://dzen.ru']")
+    SCOOTER_LOGO = (By.XPATH, "//a[@href='/']")
 
     def open(self):
-        self.driver.get(self.URL)
+        self.driver.get("https://qa-scooter.praktikum-services.ru/")
 
     def click_order_button(self, position):
-        """Клик по кнопке 'Заказать' (верхняя или нижняя)"""
         if position == "top":
-            self.driver.find_element(*self.ORDER_BUTTON_TOP).click()
+            self.click(*self.ORDER_BUTTON_TOP)
         elif position == "bottom":
-            self.driver.find_element(*self.ORDER_BUTTON_BOTTOM).click()
-
-    def click_question(self, index):
-        """Клик по вопросу в разделе 'Вопросы о важном'"""
-        questions = self.driver.find_elements(*self.FAQ_QUESTIONS)
-        questions[index].click()
-
-    def is_answer_visible(self, index):
-        """Проверка, что ответ на вопрос видим"""
-        answers = self.driver.find_elements(*self.FAQ_ANSWERS)
-        return answers[index].is_displayed()
-
-    def click_scooter_logo(self):
-        """Клик по логотипу 'Самокат'"""
-        self.driver.find_element(*self.SCOOTER_LOGO).click()
+            self.click(*self.ORDER_BUTTON_BOTTOM)
 
     def click_yandex_logo(self):
-        """Клик по логотипу 'Яндекса' и возврат URL новой вкладки"""
-        self.driver.find_element(*self.YANDEX_LOGO).click()
-        WebDriverWait(self.driver, 5).until(EC.number_of_windows_to_be(2))
-        self.driver.switch_to.window(self.driver.window_handles[1])
-        return self.driver.current_url
+        self.click(*self.YANDEX_LOGO)
+        return self.driver.window_handles[1]
+
+    def click_scooter_logo(self):
+        self.click(*self.SCOOTER_LOGO)
 
     def is_open(self):
-        pass
+        return "qa-scooter" in self.get_url()
